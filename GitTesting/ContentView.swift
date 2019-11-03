a79cee1a0fdbb5c0985814ed6e4a6859a810406f
Chris Parker <boxerflat4@iinet.net.au>
2019-11-03 18:51:04 +0800

    Initial Commit

---
+++ GitTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTesting
+//
+//  Created by Chris Parker on 3/11/19.
+//  Copyright © 2019 Chris Parker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
